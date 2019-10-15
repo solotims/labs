688d1f71d474cd66aca691406eb18c74526f1958
solotims <47323823+solotims@users.noreply.github.com>
2019-10-15 09:08:18 +0300

    Update lab2.sh

--- lab2.sh
+++ lab2.sh
@@ -5,3 +5,5 @@ sudo apt-get install python openssl libssl-dev git python-msgpack python-qt4
 git clone https://github.com/Bitmessage/PyBitmessage $HOME/PyBitmessage
 
 python2 ~/PyBitmessage/src/bitmessagemain.py
+
+# python setup.py install /usr/local/bin/pybitmessage
