4c6bf5efdd76a08f5c6a2f0c5829dff951ed6fcd
solotims <47323823+solotims@users.noreply.github.com>
2019-10-15 07:59:15 +0300

    Create lab1.sh

---
+++ lab1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+folder=$1
+rm $folder/result.xls
+echo "filename; type; modification date; size in bytes; duration" > $folder/result.xls
+
+rec()
+{
+
+(
+for i in $1/*; do
+
+if [[ -d $i ]]; then
+rec $i
+continue
+fi
+
+clean=$(basename "$i")
+filename=${clean%.*}
+
+type=$(file —mime-type -b "$i")
+
+mod=$(stat —format=%y "$i")
+
+size=$(stat —format=%s "$i")
+
+dur=$(mediainfo "$i" —Output="General;%Duration/String%")
+
+echo "$filename;$type;$mod;$size;$dur"
+
+done
+)»$folder/result.xls
+
+}
+
+rec $folder
